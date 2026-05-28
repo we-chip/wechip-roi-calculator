7103d46f9eb1035fb4239d8c9d89e655b51d26d2
WECHIP-lockers <comptes@wechip.ch>
2026-05-28 22:33:56 +0200

    ops: raise gunicorn header limits (16KB) to avoid 431 with Basic auth + cookies

--- startup.sh
+++ startup.sh
@@ -3,4 +3,7 @@ set -eu
 
 pip install --quiet --disable-pip-version-check -r requirements.txt
 
-exec gunicorn --bind=0.0.0.0:${PORT:-8000} --workers=2 --timeout=60 app:app
+exec gunicorn --bind=0.0.0.0:${PORT:-8000} --workers=2 --timeout=60 \
+  --limit-request-line 8190 \
+  --limit-request-field_size 16384 \
+  app:app
